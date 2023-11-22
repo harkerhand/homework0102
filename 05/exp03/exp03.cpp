5fe89a5d0a699097b6a792aee7823348cbe38ea7
HarkerHand <harkerhand@outlook.com>
2023-11-23 01:18:01 +0800

    汇总三个方法

--- 05/exp03/exp03.cpp
+++ 05/exp03/exp03.cpp
@@ -5,6 +5,7 @@
 using std::cout;
 using std::setw;
 
+// 仅模先前素数
 void fun1()
 {
     cout << "The prime numbers from 1 to 10000 are:\n";
@@ -37,6 +38,7 @@ void fun1()
     cout << "Total of " << ++total << " prime numbers between 1 and 10000.";
 }
 
+// 筛法排除
 void fun2()
 {
     cout << "The prime numbers from 1 to 10000 are:\n";
@@ -77,25 +79,53 @@ void fun2()
     cout << "Total of " << total << " prime numbers between 1 and 10000.";
 }
 
+// 暴力求解, 但是用平方根优化
 void fun3()
 {
+    cout << "The prime numbers from 1 to 10000 are:\n";
+    unsigned short total = 0;
+    for (unsigned short i = 2; i <= 10000; i++)
+    {
+        bool ok = 1;
+        for (unsigned short j = 2; j * j <= i; j++)
+        {
+            if (0 == i % j)
+            {
+                ok = 0;
+                break;
+            }
+        }
+        if (ok)
+        {
+            cout << setw(6) << i;
+            total++;
+            if (0 == total % 10)
+            {
+                cout << '\n';
+            }
+        }
+    }
+    cout << '\n';
+    cout << "Total of " << total << " prime numbers between 1 and 10000.";
 }
 
 int main()
 {
+    // 测试时间时候注释掉函数中cout, 可直观显示计算用时
+    // 但各方法输出手段不同, 故应该考虑带上输出的时间
     clock_t start, end;
     start = clock();
     fun1();
     end = clock();
-    cout << "\ntime of fun1 = " << double(end - start) / CLOCKS_PER_SEC << "s";
+    cout << "\ntime of fun1 = " << double(end - start) / CLOCKS_PER_SEC << "s\n";
 
     start = clock();
     fun2();
     end = clock();
-    cout << "\ntime of fun2 = " << double(end - start) / CLOCKS_PER_SEC << "s";
+    cout << "\ntime of fun2 = " << double(end - start) / CLOCKS_PER_SEC << "s\n";
 
     start = clock();
     fun3();
     end = clock();
-    cout << "\ntime of fun3 = " << double(end - start) / CLOCKS_PER_SEC << "s";
+    cout << "\ntime of fun3 = " << double(end - start) / CLOCKS_PER_SEC << "s\n";
 }
