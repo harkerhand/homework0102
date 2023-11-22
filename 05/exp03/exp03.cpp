3e5ed0afc0ece9349efb852cfd77cb804b4e1c21
HarkerHand <harkerhand@outlook.com>
2023-11-22 23:19:14 +0800

    加入计时器

--- 05/exp03/exp03.cpp
+++ 05/exp03/exp03.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 
 using std::cout;
 using std::setw;
 
 int main()
 {
+    clock_t start, end;
+    start = clock();
+
     cout << "The prime numbers from 1 to 10000 are:\n";
     unsigned short total = 0;
     unsigned short ans[10005] = {2, 0};
@@ -39,4 +43,7 @@ int main()
     }
     cout << '\n';
     cout << "Total of " << total + 1 << " prime numbers between 1 and 10000.";
+
+    end = clock(); // 结束时间
+    cout << "\ntime = " << double(end - start) / CLOCKS_PER_SEC << "s" << '\n';
 }
