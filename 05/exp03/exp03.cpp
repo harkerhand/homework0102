bd420e9b4a894c96502f75bcdb0ad51aa09dfacc
HarkerHand <harkerhand@outlook.com>
2023-11-22 23:32:31 +0800

    暴力怎么这么快

--- 05/exp03/exp03.cpp
+++ 05/exp03/exp03.cpp
@@ -11,36 +11,25 @@ int main()
     start = clock();
     cout << "The prime numbers from 1 to 10000 are:\n";
     unsigned short total = 0;
-    bool ans[10005] = {0};
     for (unsigned short i = 2; i <= 10000; i++)
     {
+        unsigned ok = 1;
         for (unsigned short j = 2; j * j <= i; j++)
         {
             if (0 == i % j)
             {
-                ans[i] = 1;
+                ok = 0;
                 break;
             }
         }
-        for (unsigned short j = 2; i * j <= 10000; j++)
+        if (ok)
         {
-            ans[i * j] = 1;
-        }
-    }
-    unsigned short count = 0;
-    for (unsigned short i = 2; i <= 10000; i++)
-    {
-
-        if (!ans[i])
-        {
-            cout << setw(6) << i << ' ';
+            cout << setw(6) << i;
             total++;
-            count++;
-        }
-        if (7 == count)
-        {
-            cout << '\n';
-            count = 0;
+            if (0 == total % 7)
+            {
+                cout << '\n';
+            }
         }
     }
     cout << '\n';
